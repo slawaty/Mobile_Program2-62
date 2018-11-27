5be166f28ed0c6f23a21997abcc8ff86eacad409
a10305-6 <slawat000@gmail.com>
2018-11-27 11:37:03 +0700

    Add picture insert music

--- app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
+++ app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
@@ -1,14 +1,22 @@
 package thanakit.ruts.ac.th.lab1_basicss;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.burbank);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+
     }
 }
 
