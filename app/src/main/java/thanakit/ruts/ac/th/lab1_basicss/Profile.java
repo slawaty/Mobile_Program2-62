eb04911ceff30db1dfdc3b3896d744924f2f3472
a10305-6 <slawat000@gmail.com>
2018-12-04 12:22:45 +0700

    introduce add button

--- app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
+++ app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Profile extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
     Button btnNext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
