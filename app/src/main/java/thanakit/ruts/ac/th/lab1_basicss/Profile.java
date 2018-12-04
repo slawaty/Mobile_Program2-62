f8787be4e73ad6ced78ab7e3019321e55250974d
a10305-6 <slawat000@gmail.com>
2018-12-04 11:49:26 +0700

    introduce add button

--- app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
+++ app/src/main/java/thanakit/ruts/ac/th/lab1_basicss/Profile.java
@@ -1,12 +1,16 @@
 package thanakit.ruts.ac.th.lab1_basicss;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,15 @@ public class Profile extends AppCompatActivity {
         mediaPlayer=MediaPlayer.create(this,R.raw.burbank);
         mediaPlayer.start();
         mediaPlayer.setLooping(true);
+
+        btnNext = (Button)findViewById(R.id.imgbtnn);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, Second.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
